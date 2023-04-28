09815e99f2210bd29f7b73a4836660fac1b9aee3
Vlad Khoteenkov <vla_kho@mail.ru>
2023-04-28 15:44:40 +0300

    Class BinOps formatted

--- src/main/java/netology/ru/BinOps.java
+++ src/main/java/netology/ru/BinOps.java
@@ -3,11 +3,11 @@ package netology.ru;
 public class BinOps {
 
     public String sum(String a, String b) {
-    int x = Integer.parseInt(a, 2);
-    int y = Integer.parseInt(b, 2);
-    int result = x + y;
-    String bynResult = Integer.toBinaryString(result);
-    return bynResult;
+        int x = Integer.parseInt(a, 2);
+        int y = Integer.parseInt(b, 2);
+        int result = x + y;
+        String bynResult = Integer.toBinaryString(result);
+        return bynResult;
     }
 
     public String mult(String a, String b) {
